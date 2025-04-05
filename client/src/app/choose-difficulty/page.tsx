bec79039c01e4f92602084794f769b37b1cd9cae
Harsh <harshtanishq2002@gmail.com>
2025-04-05 22:43:48 +0530

    chore: added animation

--- client/src/app/choose-difficulty/page.tsx
+++ client/src/app/choose-difficulty/page.tsx
@@ -32,10 +32,8 @@ function App() {
       {/* Difficulty Buttons */}
       <div className='space-y-4'>
         <button
-          className='w-full py-6 text-2xl font-bold transition-transform hover:scale-105'
+          className='w-full py-6 text-2xl font-bold transition-all duration-300 ease-in-out hover:scale-105 active:border-[3px] active:border-[#00EFCA] rounded-[10px] border border-[#757575]'
           style={{
-            borderRadius: '10px',
-            border: '1px solid #757575',
             background:
               'linear-gradient(101deg, #90FE95 -26.31%, #4E7D50 -19.97%, #49724A -15.38%, #426644 -10.33%, #1E1E1E 14.18%, #262E27 69.8%, rgba(144, 254, 149, 0.55) 207.82%)',
           }}
@@ -43,10 +41,8 @@ function App() {
           EASY
         </button>
         <button
-          className='w-full py-6 text-2xl font-bold transition-transform hover:scale-105'
+          className='w-full py-6 text-2xl font-bold transition-all duration-300 ease-in-out hover:scale-105 active:border-[3px] active:border-[#00EFCA] rounded-[10px] border border-[#757575]'
           style={{
-            borderRadius: '10px',
-            border: '1px solid #757575',
             background:
               'linear-gradient(104deg, #90D7FE -28.13%, #4E757D -21.61%, #496672 -16.89%, #426466 -11.7%, #1E1E1E 13.51%, #262A2E 70.71%, rgba(144, 236, 254, 0.55) 212.64%)',
           }}
@@ -54,10 +50,8 @@ function App() {
           MEDIUM
         </button>
         <button
-          className='w-full py-6 text-2xl font-bold transition-transform hover:scale-105'
+          className='w-full py-6 text-2xl font-bold transition-all duration-300 ease-in-out hover:scale-105 active:border-[3px] active:border-[#00EFCA] rounded-[10px] border border-[#757575]'
           style={{
-            borderRadius: '10px',
-            border: '1px solid #757575',
             background:
               'linear-gradient(104deg, #FEAB90 -28.13%, #7D4E4E -21.61%, #724D49 -16.89%, #664B42 -11.7%, #1E1E1E 13.51%, #2E2726 70.71%, rgba(254, 144, 144, 0.55) 212.64%)',
           }}
@@ -69,7 +63,7 @@ function App() {
       {/* Search Players Button */}
       <div className='mt-12 mb-10'>
         <button
-          className='w-full py-4 rounded-xl text-[#FFF] font-normal transition-all duration-300 hover:scale-105 active:scale-95 active:translate-y-1 active:shadow-none focus:outline-none focus:ring-2 focus:ring-[#3AFFE1] focus:ring-opacity-50'
+          className='w-full py-4 rounded-xl text-[#FFF] font-normal transition-all duration-300 hover:scale-105 active:scale-85 active:translate-y-1 active:shadow-none focus:outline-none focus:ring-2 focus:ring-[#3AFFE1] focus:ring-opacity-50'
           style={{
             borderRadius: '8px',
             border: '1px solid #90FE95',
